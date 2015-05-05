810e26d57fd4a848a0179b02827f99f312cab4ab
richgoldfeld <richgoldfeld@gmail.com>
2015-05-05 18:33:25 -0400

    Removed old facebook login button. Added info file ignore information.

--- index.php
+++ index.php
@@ -23,15 +23,6 @@
 </head>
 
 <body>
-    <div id="fb-root"></div><script type="text/javascript">
-(function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3&appId=348043128727600";
-        fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-    </script>
 
     <div class="site-wrapper">
         <div class="site-wrapper-inner">
