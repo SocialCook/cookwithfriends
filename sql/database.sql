8ebec03c6e547fd64b6dbdce068a5b973e060aa3
Matthew Pon <ixnoblesseobligexi@gmail.com>
2015-05-08 17:12:52 -0400

    Database SQL

--- sql/database.sql
+++ sql/database.sql
@@ -1,47 +1,29 @@
-create database cookingwithfriends;
+DROP DATABASE if exists cookingwithfriends;
+create database if not exists cookingwithfriends;
 	use cookingwithfriends;
 
-	--User
+	-- user
 	create table user(
-		u_id int NOT NULL AUTO_INCREMENT,
-		password varchar(32),
-		name varchar(255),
 		email varchar(255),
-		PRIMARY KEY (u_id),
-		UNIQUE (email)
-	);
-
-	--Kitchen objects
-	create table tools(
-		t_id int NOT NULL AUTO_INCREMENT,
-		name varchar(255),
-		description varchar(255),
-		essential int,
-		PRIMARY KEY (t_id)
-	);
-
-	--Relation between User and Kitchen Object
-	create table owns(
-		u_id int,
-		t_id int,
-		FOREIGN KEY (u_id) REFERENCES user(u_id),
-		FOREIGN KEY (t_id) REFERENCES tools(t_id),
-		PRIMARY KEY (u_id, t_id)
-	);
-
-	--Events
-	create table event(
-		e_id NOT NULL AUTO_INCREMENT,
-		edt datetime,
+		has_kitchen Boolean,
 		address varchar(255),
-		meal varchar(255)
+		PRIMARY KEY (email)
+	);
+
+	-- Events
+	create table event(
+		e_id integer AUTO_INCREMENT,
+		e_name varchar(255),
+		date_time DATETIME,
+		address varchar(255),
 		PRIMARY KEY (e_id)
 	);
-
-	create table host(
-		u_id int,
-		e_id int,
-		FOREIGN KEY (u_id) REFERENCES user(u_id),
-		FOREIGN KEY (e_id) REFERENCES event(e_id),
-		PRIMARY KEY (u_id, e_id)
+	
+	-- Attending relation between user and event
+	create table attending(
+		email varchar(255),
+		e_id integer,
+		PRIMARY KEY (email, e_id),
+		FOREIGN KEY (email) REFERENCES user(email),
+		FOREIGN KEY (e_id) REFERENCES event(e_id)
 	);
