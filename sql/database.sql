a903a04c9d8f689252f27da4851a60837c8e417f
richgoldfeld <richgoldfeld@gmail.com>
2015-05-09 00:51:33 -0400

    Updated to facebook specifications.

--- sql/database.sql
+++ sql/database.sql
@@ -1,29 +1,26 @@
 DROP DATABASE if exists cookingwithfriends;
 create database if not exists cookingwithfriends;
 	use cookingwithfriends;
-
 	-- user
 	create table user(
-		email varchar(255),
-		has_kitchen Boolean,
-		address varchar(255),
+		user_id int,
+		has_kitchen int,
+		address varchar(1000),
 		PRIMARY KEY (email)
 	);
-
 	-- Events
 	create table event(
 		e_id integer AUTO_INCREMENT,
 		e_name varchar(255),
 		date_time DATETIME,
-		address varchar(255),
+		address varchar(1000),
 		PRIMARY KEY (e_id)
 	);
-	
 	-- Attending relation between user and event
 	create table attending(
-		email varchar(255),
+		user_id int,
 		e_id integer,
-		PRIMARY KEY (email, e_id),
-		FOREIGN KEY (email) REFERENCES user(email),
+		PRIMARY KEY (user_id, e_id),
+		FOREIGN KEY (user_id) REFERENCES user(user_id),
 		FOREIGN KEY (e_id) REFERENCES event(e_id)
 	);
